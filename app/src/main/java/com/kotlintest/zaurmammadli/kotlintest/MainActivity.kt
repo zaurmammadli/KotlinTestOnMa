7cf37bf855a0708320b84b5c9c21c794d5ecdf5b
zaurmammadli <zaurmemmedli@gmail.com>
2018-08-07 12:19:32 +0400

    test

--- app/src/main/java/com/kotlintest/zaurmammadli/kotlintest/MainActivity.kt
+++ app/src/main/java/com/kotlintest/zaurmammadli/kotlintest/MainActivity.kt
@@ -2,11 +2,14 @@ package com.kotlintest.zaurmammadli.kotlintest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello Kotlin", Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/model/TestClass.java
@@ -0,0 +1,23 @@
+package model;
+
+public class TestClass {
+
+    int d;
+    String name;
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
